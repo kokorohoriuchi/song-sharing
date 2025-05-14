edc89679d8315e427ac499a5ee3479c5767a8fde
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-14 23:19:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/page1")
-def page1():
-    return "First page"
-
-@app.route("/page2")
-def page2():
-    return "Second page"

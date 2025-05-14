01835c27825cd60b96e4de3a8c1ea3e2e597cae8
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-14 23:15:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<b>Welcome</b> to the <i>app</i>!"
+    return render_template("index.html")
 
 @app.route("/page1")
 def page1():

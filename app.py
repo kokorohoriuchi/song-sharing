0caed7ab2e6b07def0d5365ed9dc10eeaf4b70f9
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-14 23:06:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "goodbye!"
+    return "Welcome to the app!"
+
+@app.route("/page1")
+def page1():
+    return "First page"
+
+@app.route("/page2")
+def page2():
+    return "Second page"

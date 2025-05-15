07027686ef9fe8292997c049a72bbd32d0cbbcda
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-15 16:25:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    words = ["apina", "banaani", "cembalo"]
-    return render_template("index.html", message="Tervetuloa!", items=words) 
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    message = request.form["message"]
+    return render_template("result.html", message=message) 

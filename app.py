eda1fddc14e2055b44d600c55d462b033a352bae
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-15 16:34:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/order")
-def order():
-    return render_template("order.html")
-
-@app.route("/result", methods=["POST"])
-def result():
-    pizza = request.form["pizza"]
-    extras = request.form.getlist("extra")
-    message = request.form["message"]
-    return render_template("result.html", pizza=pizza, extras=extras, message=message) 
+@app.route("/")
+def index():
+    return "Miten tämä <i>toimii</i>?"

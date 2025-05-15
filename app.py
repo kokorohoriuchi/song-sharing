6a163ff5ebb181318fbad147f8c911e1070e7015
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-15 16:32:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/form")
-def form():
-    return render_template("form.html")
+@app.route("/order")
+def order():
+    return render_template("order.html")
 
 @app.route("/result", methods=["POST"])
 def result():
+    pizza = request.form["pizza"]
+    extras = request.form.getlist("extra")
     message = request.form["message"]
-    return render_template("result.html", message=message) 
+    return render_template("result.html", pizza=pizza, extras=extras, message=message) 

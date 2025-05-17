a4c90d5b394b5a899d85f5cbf7011134a44be990
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-17 11:16:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 from flask import Flask
-from flask import redirect, render_template, request
-import sqlite3
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    db = sqlite3.connect("database.db")
-    messages = db.execute("SELECT content FROM messages").fetchall()
-    db.close()
-    count = len(messages)
-    return render_template("index.html", count=count, messages=messages)
-
-@app.route("/new")
-def new():
-    return render_template("new.html")
-
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    db = sqlite3.connect("database.db")
-    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
-    db.commit()
-    db.close()
-    return redirect("/")
+    db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
+    result = db.query("SELECT COUNT(*) FROM visits")
+    count = result[0][0]
+    return "Sivua on ladattu " + str(count) + " kertaa"

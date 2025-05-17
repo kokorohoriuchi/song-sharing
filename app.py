6489471021db5fb4e0139ae18020c9ca15c07111
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-17 10:56:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Miten tämä <i>toimii</i>?"
+    db = sqlite3.connect("database.db")
+    db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
+    db.commit()
+    result = db.execute("SELECT COUNT(*) FROM visits").fetchone()
+    count = result[0]
+    db.close()
+    return "Sivua on ladattu " + str(count) + " kertaa"

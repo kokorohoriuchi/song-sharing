e672efda830dd6ae853d295724d111b3557de244
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-30 20:46:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
+import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
 import db
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    messages = db.query("SELECT content FROM messages")
-    count = len(messages)
-    return render_template("index.html", count=count, messages=messages)
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
 
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]    
-    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
-    return redirect("/")
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"

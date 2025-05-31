593a57680178d129c019d4d3f4cd6c4140538838
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-05-31 13:02:23 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,22 +1,27 @@
+DROP INDEX IF EXISTS idx_thread_messages;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS threads;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    password_hash TEXT,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
     image BLOB
 );
 
 CREATE TABLE threads (
     id INTEGER PRIMARY KEY,
-    title TEXT,
-    user_id INTEGER REFERENCES users
+    title TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users
 );
 
 CREATE TABLE messages (
     id INTEGER PRIMARY KEY,
-    content TEXT,
-    sent_at TEXT,
-    user_id INTEGER REFERENCES users,
-    thread_id INTEGER REFERENCES threads
+    content TEXT NOT NULL,
+    sent_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL REFERENCES users,
+    thread_id INTEGER NOT NULL REFERENCES threads
 );
 
 CREATE INDEX idx_thread_messages ON messages (thread_id);

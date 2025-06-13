b300671b366302ee893cbde86ffa9024321437ad
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-06-13 14:34:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import markupsafe
 import config, forum, users
 import classifications
 import songs
-from db import init_app
+from db import init_db
 import os
 import click
 from flask.cli import with_appcontext

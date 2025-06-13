2794aac2e2fbf28155be6b88e62b9347d23e05fa
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-06-13 14:07:19 +0300

    Update db.py
    fixing how to initializing app

--- db.py
+++ db.py
@@ -1,24 +1,30 @@
+from flask import g, current_app
 import sqlite3
-from flask import g
 
 def get_connection():
-    con = sqlite3.connect("database.db")
-    con.execute("PRAGMA foreign_keys = ON")
-    con.row_factory = sqlite3.Row
-    return con
+    if 'db' not in g:
+        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        g.db.execute("PRAGMA foreign_keys = ON")
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+def close_connection(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_connection)
 
 def execute(sql, params=[]):
     con = get_connection()
     result = con.execute(sql, params)
     con.commit()
     g.last_insert_id = result.lastrowid
-    con.close()
 
 def query(sql, params=[]):
     con = get_connection()
-    result = con.execute(sql, params).fetchall()
-    con.close()
-    return result
+    return con.execute(sql, params).fetchall()
 
 def last_insert_id():
-    return g.last_insert_id
+    return g.get('last_insert_id')

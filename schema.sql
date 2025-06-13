570e0bb831d11408c875609034cde538fcda7f70
Kokoro Horiuchi <148705544+kokorohoriuchi@users.noreply.github.com>
2025-06-13 13:18:38 +0300

    Update schema.sql
    adding classifications tables

--- schema.sql
+++ schema.sql
@@ -20,3 +20,31 @@ CREATE TABLE messages (
 );
 
 CREATE INDEX idx_thread_messages ON messages (thread_id);
+
+CREATE TABLE IF NOT EXISTS genres (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS styles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS song_classifications (
+    song_id INTEGER NOT NULL,
+    genre_id INTEGER,
+    style_id INTEGER,
+    FOREIGN KEY (song_id) REFERENCES songs(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id),
+    FOREIGN KEY (style_id) REFERENCES styles(id),
+    PRIMARY KEY (song_id, genre_id, style_id)
+);
+
+INSERT OR IGNORE INTO genres (name) VALUES 
+('pop'), ('hiphop'), ('rap'), ('electronic'), ('indie'), 
+('country'), ('jazz'), ('R&B'), ('rock');
+
+INSERT OR IGNORE INTO styles (name) VALUES 
+('industrial'), ('blues'), ('alternative'), ('underground'), 
+('lo-fi'), ('instrumental');
